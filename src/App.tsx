abff4731fbe65fd1512498c0f4b009f66ee459f2
Sevenn <83214237+thesevenn@users.noreply.github.com>
2025-06-11 14:11:03 +0530

    init: project setup compelete

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+	return <h1 className="font-playfair text-blue-400">This is text in react</h1>;
+}
+
+export default App;
---
+++ src/components/sidebar.tsx
@@ -0,0 +1,8 @@
+import {type FC} from "react";
+
+interface PropType {}
+const Sidebar: FC<PropType> = () => {
+	return <aside>hello</aside>;
+};
+
+export default Sidebar;
